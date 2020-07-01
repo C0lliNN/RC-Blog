cf2501465495c4b2401bc47616de3097b24f0074
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2020-07-01 17:54:20 -0300

    Create View Structure
    In this application will have two views groups: One for the normal user (reader) and other for the admin (writer).
    In this way, all the views related to the user will be stored in the blog folder and the ones for the admin in the admin folder.
    There will be two layouts (one for the Reader and other for the Writer)

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+  return view('blog.index');
+})->name('blog.index');
 
 Auth::routes();
 
