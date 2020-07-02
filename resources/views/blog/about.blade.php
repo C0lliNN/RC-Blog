a68811405d48511d1d7c110f62ebd99d231c71e1
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2020-07-02 18:22:27 -0300

    Fix Lang Mistake in About View

--- resources/views/blog/about.blade.php
+++ resources/views/blog/about.blade.php
@@ -15,8 +15,7 @@ RC Blog - @lang('About')
 
 @section('content')
 <div class="col-lg-8 col-md-10 mx-auto">
-  <p>@lang('A Software Developer that loves apply his knowledge to solve problems, create powerful products and impact
-    business/lives.')
+  <p>@lang('A Software Developer that loves apply his knowledge to solve problems, create powerful products and impact business/lives.')
   </p>
   <p>@lang('I love to travel, meet new people and places. Beyond that, I also love to play video games.')</p>
 </div>
